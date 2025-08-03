a0ad089f6448e588d5281823a05eeb208c2b3da1
Cheng <cxchen0323@gmail.com>
2025-08-02 21:25:32 -0700

    From scratch

--- test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/spec'
-
-# Ensure we're loading our library files
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
-# Require base lunar module
-require 'lunar'
